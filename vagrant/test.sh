3de69d0e589de94eca6e3fd0ddec2deb14ac21d8
Sophie Kirschner <pineapplemachine@gmail.com>
2017-04-03 09:32:34 -0400

    Updated test script

--- vagrant/test.sh
+++ vagrant/test.sh
@@ -2,14 +2,18 @@ cd /migrates/migrates
 
 eval "$(pyenv init -)"
 
+touch test.log
+
 pyenv shell 2.7.13
-pip install elasticsearch==1.7.0 | sudo tee "setup-es-py.log"
+pip install elasticsearch==1.7.0 &>> "test.log"
 pyenv shell 3.6.1
-pip install elasticsearch==1.7.0 | sudo tee "setup-es-py.log"
+pip install elasticsearch==1.7.0 &>> "test.log"
+
+sudo /etc/init.d/elasticsearch stop >> "test.log"
 
 echo "  Testing with Elasticsearch 1.7.2..."
-sudo dpkg -i elasticsearch-1.7.2.deb | sudo tee "setup-es.log"
-sudo /etc/init.d/elasticsearch start | sudo tee "setup-es.log"
+sudo dpkg -i elasticsearch-1.7.2.deb &>> "test.log"
+sudo /etc/init.d/elasticsearch start >> "test.log"
 sleep 12  # Wait for ES to become available
 echo "    Testing with Python 2.7.13..."
 pyenv shell 2.7.13
@@ -17,11 +21,11 @@ python 'test/__main__.py'
 echo "    Testing with Python 3.6.1..."
 pyenv shell 3.6.1
 python 'test/__main__.py'
-sudo /etc/init.d/elasticsearch stop | sudo tee "setup-es.log"
+sudo /etc/init.d/elasticsearch stop >> "test.log"
 
 echo "  Testing with Elasticsearch 2.4.2..."
-sudo dpkg -i elasticsearch-2.4.2.deb | sudo tee "setup-es.log"
-sudo /etc/init.d/elasticsearch start | sudo tee "setup-es.log"
+sudo dpkg -i elasticsearch-2.4.2.deb &>> "test.log"
+sudo /etc/init.d/elasticsearch start >> "test.log"
 sleep 15  # Wait for ES to become available
 echo "    Testing with Python 2.7.13..."
 pyenv shell 2.7.13
@@ -29,26 +33,26 @@ python 'test/__main__.py'
 echo "    Testing with Python 3.6.1..."
 pyenv shell 3.6.1
 python 'test/__main__.py'
-sudo /etc/init.d/elasticsearch stop | sudo tee "setup-es.log"
+sudo /etc/init.d/elasticsearch stop >> "test.log"
 
 echo "  Testing with Elasticsearch 5.3.0..."
-sudo dpkg -i elasticsearch-5.3.0.deb | sudo tee "setup-es.log"
-sudo /etc/init.d/elasticsearch start | sudo tee "setup-es.log"
+sudo dpkg -i elasticsearch-5.3.0.deb &>> "test.log"
+sudo /etc/init.d/elasticsearch start >> "test.log"
 sleep 15  # Wait for ES to become available
 echo "    Testing with Python 2.7.13..."
 pyenv shell 2.7.13
 echo "      Testing with elasticsearch-py 1.7.0..."
 python 'test/__main__.py'
 echo "      Testing with elasticsearch-py 5.3.0..."
-pip install elasticsearch==5.3.0 | sudo tee "setup-es-py.log"
+pip install elasticsearch==5.3.0 &>> "test.log"
 python 'test/__main__.py'
 echo "    Testing with Python 3.6.1..."
 pyenv shell 3.6.1
 echo "      Testing with elasticsearch-py 1.7.0..."
 python 'test/__main__.py'
 echo "      Testing with elasticsearch-py 5.3.0..."
-pip install elasticsearch==5.3.0 | sudo tee "setup-es-py.log"
+pip install elasticsearch==5.3.0 &>> "test.log"
 python 'test/__main__.py'
-sudo /etc/init.d/elasticsearch stop | sudo tee "setup-es.log"
+sudo /etc/init.d/elasticsearch stop >> "test.log"
 
 echo "All done running tests!"
